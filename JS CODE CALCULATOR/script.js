3d74f5bcbf4e62e0dae6b8ce347b6e0deca700f4
ducky <68568169+DuckyTheDev1@users.noreply.github.com>
2022-06-27 12:00:48 -0400

    Add files via upload

--- JS CODE CALCULATOR/script.js
+++ JS CODE CALCULATOR/script.js
@@ -1,48 +1,40 @@
 //Function for addition
-function add (number1, number2){
+function add(number1, number2) {
     let sum = number1 + number2
     console.log(sum);
-    return sum
 }
 //Function for subtraction
-function subtract (number1, number2){
+function subtract(number1, number2) {
     let difference = number1 - number2
     console.log(difference)
-    return difference
 }
 //Function for multiplication
-function multiply (number1, number2){
+function multiply(number1, number2) {
     let product = number1 * number2
     console.log(product)
-    return product
 }
 //Function for division
-function divide (number1, number2) {
+function divide(number1, number2) {
     let quotient = number1 / number2
-console.log (quotient)
-return quotient
+    console.log(quotient)
 }
-let typeOfArithmeticOperations = prompt ("What type of arithmetic operation are you using? " + "Example: Addition or addition, etc.");
-let inputNumberOne = Number(prompt ("Enter the first number"))
-let inputNumberTwo = Number(prompt ("Enter the second number"))
- switch (typeOfArithmeticOperations) {
-    case "Addition" :
-    case "addition" :
-        add (inputNumberOne, inputNumberTwo);
+let typeOfArithmeticOperations = prompt("What type of arithmetic operation are you using? " + "Example:  + =addition , - =subtraction , * =multiplication , / =division")
+let inputNumberOne = Number(prompt("Enter the first number"))
+let inputNumberTwo = Number(prompt("Enter the second number"))
+switch (typeOfArithmeticOperations) {
+    case "+":
+        add(inputNumberOne, inputNumberTwo);
         break;
-        case "Subtraction":
-        case "subtraction" :
-            subtract (inputNumberOne,inputNumberTwo)
-             break
-            case "Multiplication" :
-            case "multiplication" :
-                multiply (inputNumberOne, inputNumberTwo)
-                break;
-                case "Division" : 
-                case "division" :
-                    divide (inputNumberOne, inputNumberTwo)
-                    break;
-                    default: console.log("Input Error, Refresh Page")
- }
- 
+    case "-":
+        subtract(inputNumberOne, inputNumberTwo)
+        break
+    case "*":
+        multiply(inputNumberOne, inputNumberTwo)
+        break;
+    case "/":
+        divide(inputNumberOne, inputNumberTwo)
+        break;
+    default: console.log("Input Error, Refresh Page")
+}
+
 
